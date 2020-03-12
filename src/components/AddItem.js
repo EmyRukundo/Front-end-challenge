14cf5547edd405adcc294523d6b4e245256d6eb4
EmyRukundo <emmanuelrukundo@Emmanuels-MacBook-Pro-3.local>
2020-03-12 21:03:07 +0200

    add new item

---
+++ src/components/AddItem.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import '../App.css';
+
+export default function AddItem() {
+    return (
+        <div className='add-item-container'>
+            <div className="button-area">
+                 <button className="add-item">Add Item</button>
+            </div>
+        </div>
+    )
+}
